87d8db5febae30eb915434cc325d3cf9b5e0c692
andy <a5706617@gmail.com>
2023-01-16 21:43:07 +0800

    commit

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,7 @@
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+  }
+  
+  const appleJuice = fruitProcessor(5, 0);
+  console.log(appleJuice);
